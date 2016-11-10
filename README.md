74df093691e5bd62a0bc3b83eb8943e05424dba5
Debanshi Bheda <debanshibheda@gmail.com>
2016-11-10 11:04:11 -0800

    added favicon

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 This application uses D3.js and the Spotify API to visually explore new artists.
 
 First, a user enters a favorite artist's name. Then, they can interact with the D3 tree to discover and listen to artists with similar styles.
-
-To run this application, simply open the index.html file in your browser.
--- server.js
+++ server.js
@@ -4,18 +4,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
  
-
 var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
-// app.use(express.static(__dirname + '/data'));
 app.use(express.static(__dirname + '/browser'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
-var exports = module.exports = {};
-
 app.get('/', function(req, res, next){
-	console.log(__dirname);
 	res.sendFile(path.join(__dirname + '/index.html'))
 })
 
