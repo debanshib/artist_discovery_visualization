ff4e1fc2619a813c27971e398f89b1a2c7b6c994
Debanshi Bheda <debanshibheda@gmail.com>
2016-11-10 11:09:09 -0800

    added favicon

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res, next){
 	res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-var port = 1337;
+var port = 3000;
 app.listen(port, function() {
     console.log('SERVER listening on port: ' + port);
 });
